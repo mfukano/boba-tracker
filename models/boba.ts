77a113e2f1766e8e3cd4e3e8f4f3cc9033d57d5d
Mat Fukano <fukano.mat@gmail.com>
2024-01-25 22:03:59 -0800

    Revert "Trying to set up dev environment for safety"
    This reverts commit 1a090604bc9e1f066215f997394c8dde5d1bef7d.

--- models/boba.ts
+++ models/boba.ts
@@ -1,12 +1,6 @@
-/** 
-    For handling numbers as it pertains to money within the browser, it's
-    far easier and more predictable to use strings than it is to parse floats
-    to given decimal points for some reason. Thus, we'll create float strings
-    to represent the price for each drink.
- */
 export interface Boba {
     flavor: string;
-    price: string;
+    price: number;
     vendor: string;
     purchase_date: string; 
 }
--- models/index.ts
+++ models/index.ts
@@ -1,24 +1,6 @@
-import { load } from "$std/dotenv/mod.ts";
 import postgres from "https://deno.land/x/postgresjs@v3.4.3/mod.js";
 
-const isDev = Deno.env.get("ENVIRONMENT") || null;
-let url = Deno.env.get("POSTGRES_URL") || null;
-let env = null;
-
-/** 
-    I'm trying to be tricky here and set environment flags to bifurcate
-    data sources between `dev` and `prod` but this isn't working so I'll
-    do something bad and operate off of production data for now.
-
-if (isDev) {
-    console.log(Deno.env.toObject());
-    env = await load({ envPath: "./.env.local" });
-    url = env["POSTGRES_URL"]; 
-}
- */
-
-console.log(`sql url: ${url}`)
-console.log(`env: ${JSON.stringify(env)}`)
+const url = Deno.env.get("POSTGRES_URL");
 
 const sql = postgres(url, {
     connect_timeout: 10,
