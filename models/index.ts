e3d5039ee39ca04b2a2313de6e809ef456180ce0
Mat Fukano <fukano.mat@gmail.com>
2024-01-25 19:17:22 -0800

    Simplify Postgres connection for 0.1

--- models/index.ts
+++ models/index.ts
@@ -1,15 +1,6 @@
 import postgres from "https://deno.land/x/postgresjs@v3.4.3/mod.js";
-import { load } from "https://deno.land/std@0.211.0/dotenv/mod.ts";
 
-const env = await load({ envPath: "./.env.production.local" });
-
-const host = env["POSTGRES_HOST"]
-const database = env["POSTGRES_DATABASE"]
-const password = env["POSTGRES_PASSWORD"]
-const user = env["POSTGRES_USER"]
-const url = env["POSTGRES_URL"]
-
-console.log(`SQL URL TEST: ${url}`)
+const url = Deno.env.get("POSTGRES_URL");
 
 const sql = postgres(url, {
     connect_timeout: 10,
