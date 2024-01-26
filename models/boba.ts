1a090604bc9e1f066215f997394c8dde5d1bef7d
Mat Fukano <fukano.mat@gmail.com>
2024-01-25 21:58:23 -0800

    Trying to set up dev environment for safety

--- models/boba.ts
+++ models/boba.ts
@@ -1,6 +1,12 @@
+/** 
+    For handling numbers as it pertains to money within the browser, it's
+    far easier and more predictable to use strings than it is to parse floats
+    to given decimal points for some reason. Thus, we'll create float strings
+    to represent the price for each drink.
+ */
 export interface Boba {
     flavor: string;
-    price: number;
+    price: string;
     vendor: string;
     purchase_date: string; 
 }
