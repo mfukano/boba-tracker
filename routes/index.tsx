174fd5b7cb33fe744fc7fe7fcdd96334a5493a1a
Mat Fukano <fukano.mat@gmail.com>
2024-02-24 19:16:07 -0800

    Include PurchaseTable islands behavior and simplify routes

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,23 +1,6 @@
-import { FreshContext, Handlers, PageProps } from "$fresh/server.ts";
+import { FreshContext } from "$fresh/server.ts";
 import { getBobaPageProps } from "../api/boba.ts";
 import PurchaseTable from "../islands/PurchaseTable.tsx";
-import { Boba } from "../models/boba.ts";
-
-export const handler: Handlers<Boba> = {
-    async GET(req: Request, ctx: FreshContext) {
-
-        // -- DEBUG -- 
-        // console.log(`ctx: `);
-        // console.log(ctx);
-
-        // console.log(`req: `);
-        // console.log(req);
-        // -- END DEBUG
-        
-        const { drinks, avgCost } = await getBobaPageProps();
-        return ctx.render({ drinks, avgCost });
-    }
-}
 
 /**
  * This page renders components related to purchased line items and a simple aggregate display.
@@ -32,23 +15,12 @@ export const handler: Handlers<Boba> = {
  * @param props.avgCost    calculated in api/boba.ts
  * @returns 
  */
-export default function Page(props: PageProps) {
-    const { drinks, avgCost } = props.data;
+// export default async function Page(props: PageProps) {
+export default async function Page(_req: Request, _ctx: FreshContext) {
+    const { drinks, avgCost } = await getBobaPageProps()
     return (
         <div className="wrapper">
-            <PurchaseTable>
-                <tbody style={"border: none"}>
-                    {drinks.map((drink: Boba, idx: number) => (
-                        <tr key={idx}>
-                            <td class="table-cell">{drink.flavor}</td>
-                            <td class="table-cell">{drink.vendor}</td>
-                            <td class="table-cell">${drink.price.toFixed(2)}</td>
-                            <td class="table-cell">{drink.purchase_date}</td>
-                        </tr>
-                    ))}
-                </tbody>
-
-            </PurchaseTable>
+            {drinks && <PurchaseTable drinks={drinks!} />}
             <div className="container col-span-full w-3/4 m-auto p-4 text-center"  >
                 <p class="rounded-lg rounded-b-none border-slate-950 bg-green-300 font-medium border-2 ml-3 p-1">Average cost: </p>
                 <p class="text-lg font-medium rounded-t-none rounded-md border-slate-950 border-r-2 border-l-2 border-b-2 border-t-0 ml-3 p-1">
