7344d283954c7c3aaba9f8f1c48e7bae5fe6fa71
Mat Fukano <fukano.mat@gmail.com>
2024-02-24 19:16:39 -0800

    Testing doesn't work because of async ops issue, ugh

--- tests/main_test.ts
+++ tests/main_test.ts
@@ -1,7 +1,8 @@
+import "$std/dotenv/load.ts";
 import { createHandler, ServeHandlerInfo } from "$fresh/server.ts";
 import manifest from "../fresh.gen.ts";
 import config from "../fresh.config.ts";
-import { assertEquals } from "$std/assert/assert_equals.ts";
+import { assert, assertEquals } from "$std/assert/mod.ts";
 
 const CONN_INFO: ServeHandlerInfo = {
     remoteAddr: { hostname: "127.0.0.1", port: 53496, transport: "tcp" }
@@ -16,8 +17,33 @@ Deno.test("HTTP assert test.", async (t) => {
         to complete behavior and unit testing but the default from the example doesn't work
         https://fresh.deno.dev/docs/examples/writing-tests
     */ 
-    await t.step("#1 GET /", async () => {
-        const resp = await handler(new Request("http://127.0.0.1/"), CONN_INFO);
+    await t.step("#1 GET /", async (t) => {
+        console.log(`running ${t.name}`)
+        const resp = await handler(new Request("http://localhost:10101/"), CONN_INFO)
+        .then(resp => {
+            if (Object.keys(resp).length < 1) {
+                console.error("No response from Request, error")
+            }
+            console.log(`log response: 
+                headers: ${
+                    JSON.stringify(resp.headers)
+                }
+                status: ${
+                    resp.status
+                }
+            `) 
+            return resp
+        });
+
         assertEquals(resp.status, 200);
     });
+
+    // tmp
+    
+      await t.step("#2 GET /foo", async () => {
+        const resp = await handler(new Request("http://127.0.0.1/"), CONN_INFO);
+        const _text = await resp.text();
+        // assert(text.includes("<h1>Boba Budget</h1>"));
+        assert(true)
+      });
 });
