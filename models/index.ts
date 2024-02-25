98a3ede50995dab7cb5b14a8d896b68d6fe3e3d1
Mat Fukano <fukano.mat@gmail.com>
2024-02-24 19:11:52 -0800

    Update Deno.env calls

--- models/index.ts
+++ models/index.ts
@@ -1,11 +1,10 @@
 import postgres from "https://deno.land/x/postgresjs@v3.4.3/mod.js";
 
-const url = await Deno.env.get("POSTGRES_URL")!;
+const url = Deno.env.get("POSTGRES_URL")!;
 
 console.log(`check PG_URL: ${url}`)
 
 const sql = postgres(url, {
-    connect_timeout: 10,
 });
 
 export default sql; 
